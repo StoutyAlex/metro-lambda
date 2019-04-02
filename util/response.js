2f24a16f81a9833c84ed6e9fc931301d2faf1b73
Alex Stout <alexstout009@hotmail.com>
2019-04-02 11:23:46 +0100

    fix(response): added headers to fix CORB/CORS

--- util/response.js
+++ util/response.js
@@ -1,8 +1,15 @@
+const headers = {
+  'X-Content-Type-Options': 'nosniff',
+  'access-control-allow-headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+  'access-control-allow-methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
+  'access-control-allow-origin': '*',
+}
 
 module.exports = {
   send: (callback, data) => callback(null, {
     statusCode: 200,
     isBase64Encoded: false,
+    headers,
     body: JSON.stringify({
       success: true,
       data,
@@ -15,6 +22,7 @@ module.exports = {
     callback(null, {
       statusCode: code,
       isBase64Encoded: false,
+      headers,
       body: JSON.stringify({
         success: false,
         error: err,
