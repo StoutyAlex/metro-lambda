269c81e160e5587a9265ab2a40915c94020d1625
Alex Stout <alexstout009@hotmail.com>
2019-04-05 09:59:49 +0100

    Added dotenv

--- metrolink/01-GetData.js
+++ metrolink/01-GetData.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const axios = require('axios');
 
 module.exports = async () => {
   return await axios.get('https://api.tfgm.com/odata/Metrolinks', {
     headers: {
-      'Ocp-Apim-Subscription-Key': process.env.METRO_API, 
+      'Ocp-Apim-Subscription-Key': process.env.METRO_API
     }
   });
 };
