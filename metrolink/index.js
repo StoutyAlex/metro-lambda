4ac7d755614cc15406d8c928cf7b8edf9fd15c50
Alex Stout <alexstout009@hotmail.com>
2019-04-05 23:40:05 +0100

    Implemented metrolink step

--- metrolink/index.js
+++ metrolink/index.js
@@ -1,17 +1,20 @@
 const getData = require('./01-GetData');
 const getStation = require('./02-GetStation');
-const getSide = require('./03-GetSides');
-const getWaitTimes = require('./04-GetWaitTimes');
-const getGrouped = require('./05-Group');
+const getMetadata = require('./03-Metadata');
+const getSide = require('./04-GetSides');
+const getWaitTimes = require('./05-GetWaitTimes');
+const getGrouped = require('./06-Group');
 
 const groupedStation = (stationName) => getData()
   .then(data => getStation(data.data.value, stationName))
+  .then(data => getMetadata(data, stationName))
   .then(data => getSide(data))
   .then(data => getWaitTimes(data))
   .then(data => getGrouped(data));
 
 const seperateStation = (stationName) => getData()
   .then(data => getStation(data.data.value, stationName))
+  .then(data => getMetadata(data, stationName))
   .then(data => getSide(data))
   .then(data => getWaitTimes(data));
 
