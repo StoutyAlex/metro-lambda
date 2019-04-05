3f1a99515232dfcf4ce19635a1b10a5e54a508c0
Alex Stout <alexstout009@hotmail.com>
2019-04-05 10:00:12 +0100

    Created script to get stationLines

---
+++ scripts/stationLineScraper.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const axios = require('axios');
+const fs = require('fs');
+const _ = require('lodash');
+
+console.log('Make sure this is ran in the TOP level directory');
+
+const getData = async () => {
+  return await axios.get('https://api.tfgm.com/odata/Metrolinks', {
+    headers: {
+      'Ocp-Apim-Subscription-Key': process.env.METRO_API
+    }
+  });
+};
+
+const run = async () => {
+  const stationLines = {};
+  const data = await getData();
+  const { value } = data.data;
+
+  value.forEach((stop) => {
+    stationLines[_.camelCase(stop.StationLocation)] = stop.Line;
+  });
+  
+  fs.writeFileSync('./data/stationLines.json', JSON.stringify(stationLines, null, 2));
+};
+
+run();
